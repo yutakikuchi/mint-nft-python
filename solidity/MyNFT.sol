69cfc951a941571cce3eb229d1f0ebef2908ee08
yutakikuchi <yuta.kikuchi@gmail.com>
2022-03-01 05:53:16 +0900

    modify class name

--- solidity/MyNFT.sol
+++ solidity/MyNFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract BelifeNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
